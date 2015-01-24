16000d1f6078f139c2fd2dc366d7464a7689f1f3
Redbeard Duadikos <redbeard@duadikos.com>
2015-01-24 13:10:07 -0800

    conveyor done

--- ggj2015 Unity Project/Assets/s_conveyor.cs
+++ ggj2015 Unity Project/Assets/s_conveyor.cs
@@ -4,38 +4,48 @@ using System.Collections;
 public class s_conveyor : MonoBehaviour {
 
 	public GameObject ingredient;
+	private float lastObjectTime;
+	public float conveyorSpeed;
+	public float conveyorLength;
+	public float conveyorSpawnDelay;
 	// Use this for initialization
 	void Start () {
-	
+		lastObjectTime = Time.realtimeSinceStartup;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		createIngredient();
+		advanceIngredients();
+		trash();
 	}
 
 	//if child is picked up, then release
 
 	//create new object every x tics
 	private void createIngredient(){
-		GameObject i_ingredient = Instantiate(ingredient,
-		                                      dispensor(),
-		                                      new Quaternion(0, 0, 0, 0)) as GameObject;
-		i_ingredient.transform.parent = gameObject.transform;
-
+		float now = Time.realtimeSinceStartup;
+		float timeSinceLast = now - lastObjectTime;
+		if (timeSinceLast > conveyorSpawnDelay){
+			lastObjectTime = now;
+			GameObject i_ingredient = Instantiate(ingredient,
+			                                      dispensor(),
+			                                      new Quaternion(0, 0, 0, 0)) as GameObject;
+			i_ingredient.transform.parent = gameObject.transform;
+		}
 	}
 
 	//move objects to the right every tic
 	private void advanceIngredients(){
-		foreach (Transform t in gameObject.transform.GetComponentsInChildren<Transform>()) {
-			t.localPosition = advancement(transform.localPosition);
+		foreach (Transform child in gameObject.transform){
+			child.localPosition = advancement(child.transform.localPosition);
 		}
 	}
 
 	//destroy any objects that are too far right
 	private void trash(){
-		foreach (Transform child in gameObject.transform.GetComponentInChildren<Transform>()){
-			if (child.localPosition.y > 10) {
+		foreach (Transform child in gameObject.transform){
+			if (child.transform.localPosition.x > conveyorLength){
 				GameObject.Destroy(child.gameObject);
 			}
 		}
@@ -48,7 +58,7 @@ public class s_conveyor : MonoBehaviour {
 
 	//determines the advancement speed of the conveyor belt
 	private Vector3 advancement(Vector3 current){
-		return current + new Vector3(0,1,0);
+		return current + new Vector3(conveyorSpeed,0,0);
 	}
 		                                  
 }
