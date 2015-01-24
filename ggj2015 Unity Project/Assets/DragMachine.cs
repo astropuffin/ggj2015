3359c7685bb4707a2e96d82cfb2eab0eb97d2c90
Kelly MacNeill <pyromuffin@gmail.com>
2015-01-24 15:14:43 -0500

    updating prefab

---
+++ ggj2015 Unity Project/Assets/DragMachine.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragMachine : MonoBehaviour {
+
+    public Collider2D dragTarget;
+    public GameObject itemHolder;
+
+
+	void Update () {
+
+        var mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log(mousePoint);
+        Debug.Log(Input.mousePosition);
+
+
+        if(Input.GetMouseButtonDown( 0 ))
+        {
+            dragTarget = Physics2D.OverlapPoint( mousePoint );
+        }
+
+        if( dragTarget != null )
+        {
+            dragTarget.transform.position = mousePoint;
+        }
+	}
+}
---
+++ ggj2015 Unity Project/Assets/Tool.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tool : MonoBehaviour {
+
+    public float useTime;
+    float timer;
+    public bool inUse;
+    public GameObject itemSpot;
+
+
+	void OnTriggerStay2D( Collider2D collider )
+    {
+        var ingredient = collider.gameObject.GetComponent<s_ingredient>();
+        if( ingredient != null && Input.GetMouseButtonUp( 0 ) )
+        {
+            ingredient.transform.parent = itemSpot.transform;
+            ingredient.transform.localPosition = Vector3.zero;
+            inUse = true;
+            timer = useTime;
+        }
+    }
+}
