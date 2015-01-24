c24ff730f4d0a7fbd9f4b747d64ef718f0d8e68e
Redbeard Duadikos <redbeard@duadikos.com>
2015-01-24 11:24:38 -0800

    adding ingredient prefab

---
+++ ggj2015 Unity Project/Assets/s_ingredient.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_ingredient : MonoBehaviour {
+
+	public string e1;
+	public string e2;
+	public string e3;
+	public string e4;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
