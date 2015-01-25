6d0e425522b55ed844305856c6486b688cab1d72
Redbeard Duadikos <redbeard@duadikos.com>
2015-01-25 14:28:16 -0800

    for reals

--- ggj2015 Unity Project/Assets/s_ingredient.cs
+++ ggj2015 Unity Project/Assets/s_ingredient.cs
@@ -14,7 +14,11 @@ public class s_ingredient : MonoBehaviour {
 		rainbowsock,
 		rubberducky,
 		shoe,
-		smartphone
+		smartphone,
+		helmet,
+		dino,
+		backpack,
+		slippers
 	}
 
 	public enum toolTypes {
@@ -36,6 +40,10 @@ public class s_ingredient : MonoBehaviour {
 	public Sprite s_rubberducky;
 	public Sprite s_shoe;
 	public Sprite s_smartphone;
+	public Sprite s_slippers;
+	public Sprite s_dino;
+	public Sprite s_backback;
+	public Sprite s_helment;
 	
 	Dictionary<ingredientType,Dictionary<toolTypes,int[]>> elements = new Dictionary<ingredientType,Dictionary<toolTypes,int[]>>();
 
@@ -73,6 +81,10 @@ public class s_ingredient : MonoBehaviour {
 		d_Sprite.Add(ingredientType.rubberducky,s_rubberducky);
 		d_Sprite.Add(ingredientType.shoe,s_shoe);
 		d_Sprite.Add(ingredientType.smartphone,s_smartphone);
+		d_Sprite.Add(ingredientType.backpack,s_backback);
+		d_Sprite.Add(ingredientType.dino,s_dino);
+		d_Sprite.Add(ingredientType.helmet,s_helment);
+		d_Sprite.Add(ingredientType.slippers,s_slippers);
 	}
 
 
@@ -101,7 +113,11 @@ public class s_ingredient : MonoBehaviour {
 		quips.Add(ingredientType.rainbowsock,new string[]{"Taste the rainbow","I made you so cool","Sock it to me!","Lost are all the happy endings","You'll go to prism for this!","But I'm recently single","You sock!","I'm a laughingsock!"});
 		quips.Add(ingredientType.gameboy,new string[]{"oh the feels","I brought you Zelda!","We gotta catch em all!","You really want to do this?","Game Over.","I'm crashing","Play Again?"});
 		quips.Add(ingredientType.legos,new string[]{"I can still build your dreams!","I'm sorry about your foot","We spent so many hours together","Remember that spaceship?","This isn't what \"lego maniac\" means","I tried picking up the pieces"});
-		quips.Add(ingredientType.smartphone,new string[]{"Siri said you loved me!","You were so excited about me!","You could sell me instead","What a phoney.","You're a loser without me","You said size doesn't matter!","Is there an app for that?",""});
+		quips.Add(ingredientType.smartphone,new string[]{"Siri said you loved me!","You were so excited about me!","You could sell me instead","What a phoney.","You're a loser without me","You said size doesn't matter!","Is there an app for that?"});
+		quips.Add(ingredientType.helmet,new string[]{"Remember how I SAVED YOUR LIFE?","I convinced mom to let you ride a bike!","Clearly I didn't protect your head well enough","Now what'll keep your toupee in place?","You owe me your life!","This all goes over my head.","Hell: met.","Did you hit your head?"});
+		quips.Add(ingredientType.backpack,new string[]{"I always had your back","I hid your porno mags!","I carried all your weight!","We've gone so far together!","I can pack it","Take me back!","Giving me the cold shoulder!"});
+		quips.Add(ingredientType.dino,new string[]{"Is it because I can't hug you?","Rawr! Translation: I love you!","Can't you see I'm trying?","It's OK, all my friends are dead anyway.","The rest is history.","Shove it up your triassic","I'm not really a tyrant!"});
+		quips.Add(ingredientType.slippers,new string[]{"You said you loved me!","I've lost that warm fuzzy feeling","What does the bunny say?","So soft... so warm","This is hare-raising.","Lettuce think about this for a moment","I'm all ears!","I'll whip my hare back and forth!"});
 	}
 
 	void populateData(){
@@ -158,6 +174,30 @@ public class s_ingredient : MonoBehaviour {
 		d_smartphone.Add(toolTypes.oven,new int[]{1,0,0,1});
 		d_smartphone.Add(toolTypes.still,new int[]{1,0,0,2});
 		elements.Add(ingredientType.smartphone,d_smartphone);
+		
+		Dictionary<toolTypes,int[]> d_dino = new Dictionary<toolTypes, int[]>();
+		d_dino.Add(toolTypes.blender,new int[]{0,0,1,0});
+		d_dino.Add(toolTypes.oven,new int[]{1,0,1,0});
+		d_dino.Add(toolTypes.still,new int[]{1,0,2,0});
+		elements.Add(ingredientType.dino,d_dino);
+
+		Dictionary<toolTypes,int[]> d_slippers = new Dictionary<toolTypes, int[]>();
+		d_slippers.Add(toolTypes.blender,new int[]{0,1,0,0});
+		d_slippers.Add(toolTypes.oven,new int[]{0,1,0,1});
+		d_slippers.Add(toolTypes.still,new int[]{1,1,0,1});
+		elements.Add(ingredientType.slippers,d_slippers);
+
+		Dictionary<toolTypes,int[]> d_backpack = new Dictionary<toolTypes, int[]>();
+		d_backpack.Add(toolTypes.blender,new int[]{0,1,0,0});
+		d_backpack.Add(toolTypes.oven,new int[]{0,1,0,1});
+		d_backpack.Add(toolTypes.still,new int[]{0,2,0,1});
+		elements.Add(ingredientType.backpack,d_backpack);
+
+		Dictionary<toolTypes,int[]> d_helmet = new Dictionary<toolTypes, int[]>();
+		d_helmet.Add(toolTypes.blender,new int[]{0,1,0,0});
+		d_helmet.Add(toolTypes.oven,new int[]{1,0,1,0});
+		d_helmet.Add(toolTypes.still,new int[]{1,0,1,1});
+		elements.Add(ingredientType.helmet,d_helmet);
 	}
 
 	void assignSprite(){
