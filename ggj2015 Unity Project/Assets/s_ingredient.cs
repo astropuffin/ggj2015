94d8109cf4f2a241e480779e71ef81787b021ba6
Redbeard Duadikos <redbeard@duadikos.com>
2015-01-24 16:20:48 -0800

    adding stuff to ingredient prefab

--- ggj2015 Unity Project/Assets/s_ingredient.cs
+++ ggj2015 Unity Project/Assets/s_ingredient.cs
@@ -1,20 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class s_ingredient : MonoBehaviour {
 
-	public string e1;
-	public string e2;
-	public string e3;
-	public string e4;
+	public enum ingredientType {
+		teddy,
+		shoe,
+		mushroom,
+		helmet
+	}
 
-	// Use this for initialization
-	void Start () {
+	public enum toolTypes {
+		boiler,
+		grinder,
+		grater
+	}
+
+	public ingredientType i_type;
+
+	//add the sprites
+	private Dictionary<ingredientType,Sprite> d_Sprite;
+	public Sprite teddy;
 	
+	Dictionary<ingredientType,Dictionary<toolTypes,int[]>> elements = new Dictionary<ingredientType,Dictionary<toolTypes,int[]>>();
+
+	void Start () {
+		populateData();
+		makeRandomIngredient();
+		assignSprite();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-}
+
+	public int[] getElements(toolTypes ttype){
+		return elements[ingredientType.teddy][ttype];
+	}
+
+	public void makeRandomIngredient(){
+		i_type = ingredientType.teddy;
+	}
+
+	void populateData(){
+		Dictionary<toolTypes,int[]> d_teddy = new Dictionary<toolTypes, int[]>();
+		d_teddy.Add(toolTypes.boiler,new int[]{1,0,0,0});
+		elements.Add(ingredientType.teddy,d_teddy);
+
+		d_Sprite.Add(ingredientType.teddy,teddy);
+
+
+
+
+//		int[,,] element_output = new int[,,]{
+//			{{1,0,0,0},{2,0,0,0},{2,1,0,0}},
+//			{{0,0,0,1},{0,0,0,2},{1,0,0,2}},
+//			{{0,0,1,0},{0,0,2,0},{0,0,2,1}},
+//			{{0,0,0,0},{0,0,0,0},{0,0,0,0}}
+//		}
+
+
+	}
+
+	void assignSprite(){
+//		GetComponent("SpriteRenderer").sprite = d_Sprite(i_type);
+		GetComponent<SpriteRenderer>().sprite = d_Sprite[i_type];
+	}
+
