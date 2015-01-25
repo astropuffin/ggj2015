f70aa8e50495ae71bc0c10522d9e39b68e7fe618
Redbeard Duadikos <redbeard@duadikos.com>
2015-01-25 13:51:16 -0800

    text timer

---
+++ ggj2015 Unity Project/Assets/textTimer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textTimer : MonoBehaviour {
+
+	private bool clockStarted;
+	private float start;
+	public float maxAge = 5f;
+	// Use this for initialization
+	void Start () {
+		clockStarted = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkClock();
+	}
+
+	public void startClock(){
+//		Debug.Log ("start clock");
+		start = Time.realtimeSinceStartup;
+		clockStarted = true;
+	}
+
+	private void checkClock(){
+		if(clockStarted){
+			float now = Time.realtimeSinceStartup;
+			float difference = now - start;
+//			Debug.Log(difference);
+			if (difference > maxAge){
+				GameObject.Destroy(this.gameObject);
+			}
+		}
+	}
+
+}
